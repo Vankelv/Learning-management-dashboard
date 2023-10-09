b7887abf25183f2da6f04667dc1184bd0796eb8a
Kelvin Samuel Klutse <88723961+Vankelv@users.noreply.github.com>
2023-10-09 02:56:46 +0000

    sidebar added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Sidebar from './components/sidebar'
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Dashboard from './pages/dashboard';
+import React from "react";
+import "react-bootstrap";
+import Sidebar from "./components/sidebar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Dashboard from "./pages/dashboard";
+import "./assets/main.css";
 const App = () => {
   return (
     <div>
-        <Container>
-            <Row className='flex'> 
-                <Col className='col-4'>
-                <Sidebar/>
-                </Col>
-                <Col className='col-8'>
-                    <Dashboard/>
-                </Col>
-            </Row>
-        </Container>
-  
+      <Container>
+        <Row className="flex">
+          <Col xs lg="6" className="sidebar fixed">
+            <Sidebar />
+          </Col>
+          <Col className="body">
+            <Dashboard />
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Client/src/components/Sidebar.jsx
+++ Client/src/components/Sidebar.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
-
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import {BiHome} from 'react-icons/bi';
+import {RxDashboard} from 'react-icons/rx';
+import {BsPlusCircle} from 'react-icons/bs';
+import {FiCopy} from 'react-icons/fi';
+import {MdOutlineAssignmentTurnedIn} from 'react-icons/md';
 const Sidebar = () => {
   return (
-    <div>sidebar</div>
+    <div >
+      <Container className='bg-gray-100  f-height'>
+        <Row className='sections ' >
+           <Col className='nav-logo'>
+           <a href='#' className='logo'>
+            <img src='imgs/logo.webp' alt='site logo'/>
+          </a>
+           <ul>
+            <li>
+            <BiHome className='icon text-gray' size={32}/>
+              <a href='#' className='link-text-gray'>Dashboard</a>
+            </li>
+          
+            <li>
+            <RxDashboard className='icon text-gray' size={32}/>
+              <a href='#' className='link-text-gray'>Add Course</a>
+            </li>
+            <li>
+            <BsPlusCircle className='icon text-gray' size={32}/>
+              <a href='#' className='link-text-gray'>Add Course</a>
+            </li>
+            <li>
+            <FiCopy className='icon text-gray' size={32}/>
+              <a href='#' className='link-text-gray'>Lessons</a>
+            </li>
+            <li>
+            <FiCopy className='icon text-gray' size={32}/>
+            <a href='#' className='link-text-gray'>Lessons</a>
+            </li>
+           </ul>
+           </Col>
+           <Col>Col 3</Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
